de616b481bdcf237ceb2c275932f519325358833
Shivam <54373797+shivam0110@users.noreply.github.com>
2021-10-02 18:47:53 +0530

    Ez codes

---
+++ GregorandCryptography.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+const ll mod = 1000 * 1000 * 1000 + 7;
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t, p;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> p;
+
+        cout << 2 << " " << p - 1 << endl;
+    }
+
+    return 0;
+}
